b52974bf44f45d098db717987146c4f9cfb0956c
dmelim <dmpmelim@gmail.com>
2022-07-13 00:21:45 +0100

    Adding Charts, dynamic styles and fixing bugs.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+import "./Chart.css"
 import ChartBar from "./ChartBar";
 
 const Chart = props => {
     
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
+    
     return (
         <div className="chart">
-            {props.dataPoints.map(datapoint => 
+            {props.dataPoints.map((dataPoint) => (
             <ChartBar
                 key={dataPoint.label}
                 value={dataPoint.value} 
-                maxValue={null} 
-                label={dataPoint.label}/>)}
+                maxValue={totalMaximum} 
+                label={dataPoint.label}/>))}
         </div>
     )
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css"
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = props => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,7 +18,9 @@ const Expenses = props => {
         <Card className="expenses">
             <ExpensesFilter
                 selected={filteredYear}
-                onFilterYearChange={filterChangeHandler} />
+                onFilterYearChange={filterChangeHandler} 
+            />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     )
