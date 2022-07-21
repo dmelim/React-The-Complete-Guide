fb12ffd2f3515f718c0c010ce8f9a842f39532b3
dmelim <dmpmelim@gmail.com>
2022-07-21 12:22:56 +0100

    Finished food order app project.

--- 05-Food-Order-Project/src/components/UI/Input.js
+++ 05-Food-Order-Project/src/components/UI/Input.js
@@ -1,14 +1,15 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* The use of "..."(spread operator) makes the input type variable dependending on what is given through "props.input". 
       See the use in MealItemForm to understand. */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
--- 05-Food-Order-Project/src/components/UI/Modal.js
+++ 05-Food-Order-Project/src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOVerlay = (props) => {
@@ -18,7 +18,10 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOVerlay>{props.children}</ModalOVerlay>,
         portalElement
