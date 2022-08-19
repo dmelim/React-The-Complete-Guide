4067a6eeda7c61c8636f150b902aed2497da6e31
dmelim <dmpmelim@gmail.com>
2022-08-19 18:45:42 +0100

    Finished the refresher.

--- 12-Advanced-Redux/src/components/Shop/Products.js
+++ 12-Advanced-Redux/src/components/Shop/Products.js
@@ -1,16 +1,30 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_DATA = [
+  { id: "p1", price: 6, title: "A BOOK", description: "The BOOK of Boks" },
+  {
+    id: "p2",
+    price: 7,
+    title: "ANOTHER BOOK",
+    description: "The BOOK of ANOUTEGR",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
