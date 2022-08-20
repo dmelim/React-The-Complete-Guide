5f82439191efe15fb0a1a931d91b56e50b556143
dmelim <dmpmelim@gmail.com>
2022-08-20 20:55:26 +0100

    Handling http states and feedback with redux

--- 12-Advanced-Redux/src/store/ui-slice.js
+++ 12-Advanced-Redux/src/store/ui-slice.js
@@ -2,11 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       // We can directly mutate the state, because in the background redux toolkit uses immer to make a copy of the state and not changing it directly.
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
