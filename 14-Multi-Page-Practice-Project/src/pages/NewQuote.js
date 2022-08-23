0d63001505b37cf91464a34a3ba6a723c00a0830
dmelim <dmpmelim@gmail.com>
2022-08-23 17:36:46 +0100

    Adding the "comments" feature.

--- 14-Multi-Page-Practice-Project/src/pages/NewQuote.js
+++ 14-Multi-Page-Practice-Project/src/pages/NewQuote.js
@@ -1,16 +1,30 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
   // enables us to access the browser history and navigate to other pages. AKA programmatic Navigation
   const history = useHistory();
+  const { sendRequest, status } = useHttp(addQuote);
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
+
     // then we use history push to redirect the user. We can also use replace, but then the user can't go to the previous page
     history.push("/quotes");
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
--- 14-Multi-Page-Practice-Project/src/pages/QuoteDetail.js
+++ 14-Multi-Page-Practice-Project/src/pages/QuoteDetail.js
@@ -1,32 +1,53 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "The best quote, maybe ever." },
-  { id: "q2", author: "Ted", text: "Literaly, is not literal sometimes." },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetails = () => {
   // what is being used as an argument in the route is what is gonna be used in "params.quoteId" so "params.argument"
   const params = useParams();
+
+  const { quoteId } = params;
+
   const match = useRouteMatch();
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-  if (!quote) {
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>Your quote wasn't Found</p>;
   }
 
   return (
     <Fragment>
       <h1>Quote Details</h1>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       {/* It dynamically updates the path, using the useRouteMatch hook. 
       If we change the absolute path, the hook automatically updates it, 
       making it easier to work with nested routes */}
-      <Route path={`${match.path}/comments`} exact>
+      <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
