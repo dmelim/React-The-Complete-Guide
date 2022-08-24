f4b6025934fb5f5fd858e30b6892e2863e1bc1f9
dmelim <dmpmelim@gmail.com>
2022-08-24 17:59:43 +0100

    Managing global state with context api, and added change password functionality.

--- 15-User-Auth/src/components/Auth/AuthForm.js
+++ 15-User-Auth/src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authctx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -50,7 +53,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authctx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
